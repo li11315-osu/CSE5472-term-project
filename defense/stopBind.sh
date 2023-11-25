e2e37528ee0a60f61e4b84719ff4bb054702ecf6
Thomas Li <li.11315@osu.edu>
2023-11-24 21:06:08 -0500

    Get server to run with rate limiting configured - not sure if rate limiting is actually working, though, considering that it has basically no effect on the direct flood

--- defense/stopBind.sh
+++ defense/stopBind.sh
@@ -1,5 +1,6 @@
 # Move the original config file back in place and stop the daemon
 sudo mv "/etc/bind/named.conf.local.original" "/etc/bind/named.conf.local"
+sudo mv "/etc/bind/named.conf.options.original" "/etc/bind/named.conf.options"
 sudo rm /etc/bind/zonefile
 sudo service named stop
 
